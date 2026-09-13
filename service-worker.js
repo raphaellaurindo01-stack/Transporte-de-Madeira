a719c1cafbca279471f73213d8e201910cc12948
raphaellaurindo01-stack <raphaellaurindo01@gmail.com>
2026-09-13 17:45:58 -0300

    Atualiza cache para matriz GPO inteligente v5

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='transporte-madeira-v4';
+const CACHE='transporte-madeira-v5';
 const CORE=['./','./index.html','./styles.css','./app.js','./decision.js','./legal-bo.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
